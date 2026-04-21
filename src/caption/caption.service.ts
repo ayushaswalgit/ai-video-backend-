d9a6b390f456fb207c0a5c8bc5c77d4901957da3
AyushhAswal <ayushaswal244@gmail.com>
2026-04-21 23:16:00 +0530

    newMethod

--- src/caption/caption.service.ts
+++ src/caption/caption.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import axios from 'axios';
@@ -6,33 +9,50 @@ import axios from 'axios';
 export class CaptionService {
   private gentAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
-  async generateCaption(imageurl: string) {
+  async generateCaption(imageUrl: string) {
     try {
-      const response = await axios.get(imageurl, {
+      if (!imageUrl) {
+        throw new Error('Image URL required');
+      }
+
+      const response = await axios.get(imageUrl, {
         responseType: 'arraybuffer',
       });
+
       const base64 = Buffer.from(response.data).toString('base64');
-      const model = this.gentAi.getGenerativeModel({
-        model: 'gemini-1.5-flash',
-      });
-      const result = await model.generateContent([
+
+      const mimeType = imageUrl.endsWith('.png') ? 'image/png' : 'image/jpeg';
+
+      const geminiResponse = await axios.post(
+        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
         {
-          inlineData: {
-            mimeType: 'image/jpeg',
-            data: base64,
-          },
+          contents: [
+            {
+              parts: [
+                {
+                  inlineData: {
+                    mimeType: mimeType,
+                    data: base64,
+                  },
+                },
+                {
+                  text: 'Generate a short Instagram caption with emojis',
+                },
+              ],
+            },
+          ],
         },
-        {
-          text: 'Generate a short Instagram caption with emojis',
-        },
-      ]);
-      const text = result.response.text();
+      );
+
+      const data = geminiResponse.data as any;
+
+      const caption = data?.candidates?.[0]?.content?.parts?.[0]?.text;
 
       return {
-        caption: text,
+        caption: caption ?? 'No caption generated',
       };
     } catch (e) {
-      console.log('Gemini error:', e);
+      console.log('FULL ERROR:', e);
       throw new Error('Caption generation failed');
     }
   }
