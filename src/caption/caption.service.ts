d2242a42163184645aca6e9518d116efe3223625
AyushhAswal <ayushaswal244@gmail.com>
2026-04-21 22:53:04 +0530

    new

--- src/caption/caption.service.ts
+++ src/caption/caption.service.ts
@@ -13,7 +13,7 @@ export class CaptionService {
       });
       const base64 = Buffer.from(response.data).toString('base64');
       const model = this.gentAi.getGenerativeModel({
-        model: 'gemini-1.5-flash',
+        model: 'gemini-1.5-flash-latest',
       });
       const result = await model.generateContent([
         {
