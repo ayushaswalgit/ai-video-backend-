1685cdfed02d2963842e333e227eeed08e0d2a1f
AyushhAswal <ayushaswal244@gmail.com>
2026-04-21 23:23:16 +0530

    again new

--- src/caption/caption.service.ts
+++ src/caption/caption.service.ts
@@ -24,7 +24,7 @@ export class CaptionService {
       const mimeType = imageUrl.endsWith('.png') ? 'image/png' : 'image/jpeg';
 
       const geminiResponse = await axios.post(
-        `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
         {
           contents: [
             {
